1928ed3163bb28577401906fdc8518b9587ac5ab
Baruch Mendivil <baruchml@hotmail.com>
2023-09-02 19:05:24 -0600

    Tercera Preentrega

--- main.js
+++ main.js
@@ -1,139 +1,194 @@
 
-//CONSTRUCTOR ALUMNOS
-class Alumno {
-    constructor(nombre, carrera, colegiatura) {
-        this.nombre = nombre;
-        this.carrera = carrera;
-        this.colegiatura = colegiatura;
-    }
-    inscrito() {
-        alert(`Felicidades ${this.nombre} estas inscrito en la carrera de 
-    ${this.carrera}, tus mensualidades seran de
-    ${this.colegiatura}, no te olvides asistir a todas clases y pagar puntualmente.`);
-    }
-}
-//GRUPOS
-const grupoDiseño = [];
-const grupoAdmin = [];
-const grupoDerecho = [];
-const colegiaturas = [2000, 1700, 2100]
-//ALUMNOS FICTICIOS
-grupoDiseño.push(new Alumno("Javier", "Diseño", colegiaturas[0]));
-grupoDiseño.push(new Alumno("Luis", "Diseño", colegiaturas[0]));
+//DA LA BIENVENIDA SOLO SI EL USUARIO DIO EN LA PESTAÑA DE RECORDARME EN EL FORM DE USUARIO
 
-grupoAdmin.push(new Alumno("Lucas", "Admin", colegiaturas[1]));
-grupoAdmin.push(new Alumno("Mariana", "Admin", colegiaturas[1]));
-
-grupoDerecho.push(new Alumno("Sol", "Derecho", colegiaturas[2]));
-grupoDerecho.push(new Alumno("Ana", "Derecho", colegiaturas[2]));
-
-//INTERACCION
-const nombreAlumno1 = prompt("¡Bienvenido! Ingresa tu nombre porfavor");
-let mensaje = (`Hola ${nombreAlumno1}, estas son las carreras dispobibles en nuestra Institucion:
-Diseño.
-Admin.
-Derecho.`);
-alert(mensaje);
-
-//infocarreras
-const infoCarreras = [
-    { nombre: "diseño", semestres: "8", colegiatura: colegiaturas[0], inicio: "6 de agosto" },
-    { nombre: "admin", semestres: "6", colegiatura: colegiaturas[1], inicio: "15 de agosto" },
-    { nombre: "derecho", semestres: "10", colegiatura: colegiaturas[2], inicio: "28 de agosto" },
+let usuarioStorage = JSON.parse(localStorage.getItem("datosUsuario"));
+if (usuarioStorage) {
+  usuarioStorage.forEach((item) => { 
+  let divSaludo = document.createElement("div");
+  divSaludo.innerHTML = `<h1>Bienvenid@ nuevamente ${item.nombre}</h1>`;
+  contenedor.append(divSaludo);
+  });
+}else {saludo.innerHTML = "<h1>Bienvenid@, Aun no has iniciado sesion. Ve a usuario / usuario</h1>";
+contenedor.innerHTML = `<h2>Los elementos que tienen puntos para calificar estan en</h2>
+<ul>
+    <li>El cambio del contenido en pantalla sin usar links</li>
+    <li>La pagina de Usuario / Usuario </li>
+    <li>La pagina de Menu / Carreras </li>
+</ul>
+<p> las demas pestañas estan en construccion :)</p>`;}
+//BTN ELIMINAR
+let eliminar = document.getElementById("eliminar");
+//SUBIMOS A LOCAL STORAGE LOS GRUPOS
+const carreras = [
+    { id: 1, nombre: "Diseño", modalidad: "Escolarizado", semestres: 8,colegiatura: 1200 },
+    { id: 2, nombre: "Arquitectura", modalidad: "Escolarizado", semestres: 10,colegiatura: 1300 },
+    { id: 3, nombre: "Administración", modalidad: "Mixto", semestres: 6 ,colegiatura: 1800},
+    { id: 4, nombre: "Programación", modalidad: "En linea", semestres: 8 ,colegiatura: 2000},
+    { id: 5, nombre: "Derecho", modalidad: "Mixto", semestres: 10,colegiatura: 1500 },
 ];
-//BUSCADOR
-let buscador = prompt(`Si escribes el nombre de la carrera (Diseño, Admin o Derecho)
-recibiras toda la informacion acerca de ella. O escribe "ESC" para avanzar`).toLocaleLowerCase();
+localStorage.setItem("carreras", JSON.stringify(carreras));
 
-while (buscador != "esc") {
+//PAGES
+let navInicio = document.getElementById("inicio");
+navInicio.addEventListener("mouseup", () => {
+    saludo.innerHTML = "<h1>Bienvenid@ a Inicio, ve a usuario / usuario para Iniciar Sesion </h1>";
+    contenedor.innerHTML = `<h2>Los elementos que tienen puntos para calificar estan en</h2>
+    <ul>
+        <li>El cambio del contenido en pantalla sin usar links</li>
+        <li>La pagina de Usuario / Usuario </li>
+        <li>La pagina de Menu / Carreras </li>
+    </ul>
+    <p> las demas pestañas estan en construccion :)</p>`
+    
+});
 
-    const info = infoCarreras.find((item) => item.nombre === buscador);
+let navUsuario = document.getElementById("usuario");
+navUsuario.addEventListener("mouseup", () => {
+    //FORM USUARIO HTML
+    contenedor.innerHTML = "";
+    saludo.innerHTML = "<h1>Bienvenid@ a usuario </h1>";
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <form id="formulario">
+    <label for = "formUsuario">Escribe tu nombre de usuario para iniciar sesion</label>
 
-    if (info) {
-        let mensajeInfo =
-        `Licenciatura en ${info.nombre}:
-        Semestres a cursar: ${info.semestres}
-        Colegiatura: ${info.colegiatura}
-        Inicia: ${info.inicio}`
+            <input type="text" id="formUsuario" placeholder="Nombre">
+                <label for = "formUsuario">Correo</label>
 
-        alert(mensajeInfo)
+            <input type="text" id="formCorreo" placeholder="example@hotmail.com">
+                <label for = "formContrasena">Contraseña</label>
 
+            <input type="text" id="formContrasena" placeholder="Password">
+        <input type="submit" value = "Enviar">
+        <input type="checkbox" id="formCheckbox">
+        <label for = "formCheckbox"> Recordarme </label>
+        <div>-------</div>
+        <div>
+        <button id="eliminar">ELIMINAR USUARIO</button>
+        </div>
+        
+        
+</form>`;
+    contenedor.append(div);
+    
+    //FORM USUARIO JSON
+    let formulario = document.getElementById("formulario");
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault(); 
+        let inputs = e.target.children;
+
+        //DEPENDIENDO DE LA RESPUESTA SUBIREMOS INFORMACION A SESSION O A LOCAL STORAGE. TAMBIEN DETERMINA SI ESTA MAL ESCRITO
+            if (inputs[1].value.includes("@") || !inputs[3].value.includes("@")) {
+                inputs[1].value = "";
+                inputs[3].value = "";
+                inputs[5].value = "";
+                
+                alert("Tu nombre o el correo estan mal escritos");
+
+            } else if (inputs[7].checked === true) {
+                alert(`Bienvenid@ por primera vez ${inputs[1].value}`);
+                const datosUsuario = [{ nombre: inputs[1].value, correo: inputs[3].value, contrasena: inputs[5].value }];
+                localStorage.setItem("datosUsuario", JSON.stringify(datosUsuario));
+            } else {
+                alert(`Bienvenid@ por unica vez ${inputs[1].value}`);
+                const datosUsuario = [{ nombre: inputs[1].value, correo: inputs[3].value, contrasena: inputs[5].value }];
+                sessionStorage.setItem("datosUsuario", JSON.stringify(datosUsuario));          
+
+            };
+        //SÍ ELIMINA, PERO EL FORM VUELVE A DAR EL SUMBIT JUSTO DESPUES Y ENTONCES SE CARGA DE NUEVO LA INFO. AYUDA
+            let eliminar = document.getElementById("eliminar");
+            eliminar.addEventListener("click", () => {
+            localStorage.removeItem("datosUsuario");//NO QUIERO QUE ME BORRE TODO POR ESO SOLO USO EL REMOVE ITEM, Y NO CLEAR
+            sessionStorage.removeItem("datosUsuario");
+            location.reload();
+            alert("Usuario eliminado con exito");});
+
+        });
+    });
+let navCarreras = document.getElementById("carreras");
+const renderizarCarreras = ()=>{
+    //NO HE PIDIDO HACER QUE FUNCIONE ESTE SELECT
+    contenedor.innerHTML = `<label
+    >Elija una modaldad:
+    <select class="nieve" name="nieve" id="tipoModalidad">
+      <option selected value="0">Seleccione Uno …</option>
+      <option value="Escolarizado">Escolarizado</option>
+      <option value="Mixto">Mixto</option>
+      <option value="En linea">En linea</option>
+    </select>
+  </label>`;
+
+    saludo.innerHTML = "<h1>Bienvenid@ las carreras </h1>";
+
+    let carrerasDisponibles = JSON.parse(localStorage.getItem("carreras"));
+   
+    carrerasDisponibles.forEach((carrera) => {
+        let div = document.createElement("div");
+        div.innerHTML = `<div class="card">
+            <div class="card">
+             <h3 class="card-header">${carrera.nombre}</h3>
+              <div class="card-body">
+                 <h5 class="card-title">Modalidad: ${carrera.modalidad}</h5>
+                 <p class="card-text">Cantidad de semestres a cursar: ${carrera.semestres}.</p>
+                 <p class="card-text">Colegiatura: ${carrera.colegiatura}.</p>
+                 <a href="#" class="btn btn-primary">Mirar más</a> 
+                 <a href="#" class="btn btn-primary id="btnEscoger">Escoger</a>
+              </div>
+            </div>      
+`
+        contenedor.append(div);    
+    });
+};
+
+navCarreras.addEventListener("mouseup", () => {
+    renderizarCarreras();
+     
+});
+
+/*QUIERO RENDERIZAR CON EL SELECT DE ARRIBA PARA FILTRAR LAS CARDS DE LAS CARRERAS POR MODALIDAD PERO NO LO HE CONSEGUIDO
+
+const selectorModalidad = document.getElementById("tipoModalidad")
+selectorModalidad.onchange = (evt)=>{
+    const modalidadSeleccionada =  evt.target.value
+    if(modalidadSeleccionada === "0"){
+        renderizarCarreras(carreras);
     } else {
-        alert("No se encontro la carrera");    
+        renderizarCarreras(carreras.filter(prod=>prod.type === modalidadSeleccionada))
     }
-    buscador = prompt(`Si escribes el nombre de la carrera (Diseño, Admin, Derecho)
-    recibiras toda la informacion acerca de ella o escribe "ESC" para avanzar`).toLocaleLowerCase();
-
-}
-//SELECCION DE CARRERA
-const carreraAlumno1 = prompt(`Si quieres elegir una carrera solo tienes que escribir su nombre
-(Diseño, Admin, Derecho)`).toLowerCase();
-
-switch (carreraAlumno1) {
-    case "diseño":
-        const alumnoDis = new Alumno(nombreAlumno1, carreraAlumno1, colegiaturas[0]);
-        grupoDiseño.push(alumnoDis);
-        alumnoDis.inscrito();
-        break;
-    case "admin":
-        const alumnoAdm = new Alumno(nombreAlumno1, carreraAlumno1, colegiaturas[1]);
-        grupoAdmin.push(alumnoAdm);
-        alumnoAdm.inscrito();
-        break;
-    case "derecho":
-        const alumnoDer = new Alumno(nombreAlumno1, carreraAlumno1, colegiaturas[2]);
-        grupoDerecho.push(alumnoDer);
-        alumnoDer.inscrito();
-        break;
-
-    default:
-        alert("No se encuentra esa carrera");
-        break;
-}
-//BECA
-let beca = prompt(`Si eres acredor de una Beca aqui te dejamos los costos a pagar
-solo elige si es por Promedio o de Gobierno o usa "ESC" para avanzar`).toLocaleLowerCase();
-
-switch (beca) {
-    case "gobierno":
-        const becaGob = colegiaturas.map ((item) =>
-            item - item * .30);
-            let mensajeBecaGob = `
-            Diseño antes($2000). CON BECA GOBIERNO! ${becaGob[0]} 
-            Admin antes($1700). CON BECA GOBIERNO! ${becaGob[1]} 
-            Derecho antes($2100). CON BECA GOBIERNO! ${becaGob[2]} `
-            
-            alert(mensajeBecaGob);
-        break;
-    case "promedio":
-        const  becaProm = colegiaturas.map ((item) =>
-            item - item * .50);
-            let mensajeBecaProm = `
-            Diseño antes($2000). CON BECA POR PROMEDIO! ${becaProm[0]} 
-            Admin antes($1700). CON BECA POR PROMEDIO! ${becaProm[1]} 
-            Derecho antes($2100). CON BECA POR PROMEDIO! ${becaProm[2]} `
-            
-            alert(mensajeBecaProm);
-        break;
-
-    default:
-        alert("Las becas se pueden adquirir en cualquier semestre asi que no te preocupes y da lo mejor de ti!")
-        break;
-}
+}*/
 
 
-//PARA USO INTERNO, COMPROBACION DE QUE LOS ALUMNOS ESTEN INTEGRADOS EN LOS GRUPOS
-grupoDiseño.forEach(item => {
-    console.log(item);
+
+let navPerfil = document.getElementById("perfil");
+navPerfil.addEventListener("mouseup", () => {
+    saludo.innerHTML = "<h1>Bienvenid@ a tu perfil </h1>";
+    contenedor.innerHTML = `
+    <a href="#" class="btn btn-primary id="tuGrupo">Tu Grupo</a>
+    </div>`
+    //EN PERFIL QUIERO TRAER INFORMACION DE CUANDO EL USUARIO INICIE SESION ("usuarioDatos") E INFORMACION DE LA CARRERA QUE ELIGA
+    //EN LAS CARDS DE LAS CARRERAS VIENE EL BTN "ESCOGER". CON ESO PUEDO TRAER LA INFO DE ("carreras")
+    //UNIENDO SOLO LOS DATOS QUE YO NECESITO DE AMBOS ARRAYS QUIERO CREAR TU PERFIL. ESE PERFIL METERLO A UN GRUPO.
+
+    //RESUMIENDO:
+    //QUIERO SACAR EL VALOR DE UN OBJETO QUE ESTE EN UN ARRAY QUE ESTE EN LOCALSTORAGE. PARA HACER UN OBJETO NUEVO.
+    //ME PUEDE ORIENTAR DE COMO DEBERIA HACERLO PORFAVOR.
 });
 
-grupoAdmin.forEach(item => {
-    console.log(item);
+//PESTAÑAS EN CONSTRUCCION:
+let navCampus = document.getElementById("campus");
+navCampus.addEventListener("mouseup", () => {
+    saludo.innerHTML = "<h1>Bienvenid@ a campus </h1>";
+    contenedor.innerHTML = ""
 });
-grupoDerecho.forEach(item => {
-    console.log(item);
+
+let navNoticias = document.getElementById("noticias");
+navNoticias.addEventListener("mouseup", () => {
+    saludo.innerHTML = "<h1>Bienvenid@ a noticias </h1>";
+    contenedor.innerHTML = ""
 });
-
-
-
+let navNosotros = document.getElementById("nosotros");
+navNosotros.addEventListener("mouseup", () => {
+    saludo.innerHTML = "<h1>Bienvenid@ a nosotros </h1>";
+    contenedor.innerHTML = ""
+});
 
